a509757225ad123735e1917d76434432f9f7e2c3
Gloria Lim <glorialimartt@gmail.com>
2023-10-21 23:12:29 -0400

    Submit Pair Programming assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,42 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Pair programmed by Jordan atkins - Github @Atkinss1
+// Pair programmed by Gloria Lim - Github @glowiep
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return "The matrix is empty.";
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+    else if (l.split("").reverse().join("").includes(word)) {
+      return true;
+    }
+  }
+
+  let row = letters.length;
+  let column = letters[0].length;
+  let newMatrix = [];
+
+  for (let i = 0; i < column; i++) {
+    let newRow = [];
+    for (let j = 0; j < row; j++) {
+      newRow.push(letters[j][i]);
+    }
+    newMatrix.push(newRow);
+  }
+
+  const verticalJoin = newMatrix.map(ls => ls.join(''));
+
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+    else if (l.split("").reverse().join("").includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
